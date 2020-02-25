29d3a3bb58a087152a4a1e4ebd59f953194583a7
gyl-dev <gyl-dev@posteo.eu>
2020-02-25 11:51:29 +0100

    Lazy load AWS config

--- src/getAWS.js
+++ src/getAWS.js
@@ -1,11 +1,11 @@
-require('dotenv').config()
 const AWS = require('aws-sdk')
 
 // Create default connection details for AWS API
-AWS.config = new AWS.Config({
-	region: process.env.AWS_REGION,
-	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-})
-
-module.exports = () => AWS
+module.exports = () => {
+	AWS.config = new AWS.Config({
+		region: process.env.AWS_REGION,
+		accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+	})
+	return AWS;
+}
