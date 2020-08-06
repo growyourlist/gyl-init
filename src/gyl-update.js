b77798c3c851999b21f49196076f1889ab2e7573
gyl-dev <gyl-dev@posteo.eu>
2020-08-06 19:47:36 +0200

    Turn on full-update

--- src/gyl-update.js
+++ src/gyl-update.js
@@ -34,159 +34,157 @@ const showChange = (paramName = '', oldValue = '', newValue = '') => {
 
 const doUpdate = async () => {
 	try {
-		if (false) {
-			Logger.log(
-				'\n### UPDATING GROW YOUR LIST (GYL) ###\n\n' +
-					'This program will update GYL in your AWS account.'
+		Logger.log(
+			'\n### UPDATING GROW YOUR LIST (GYL) ###\n\n' +
+				'This program will update GYL in your AWS account.'
+		);
+		await loadConfig();
+		const cloudformation = new AWS.CloudFormation();
+		const existingStackResponse = await cloudformation
+			.describeStacks({ StackName: 'GrowYourList' })
+			.promise();
+		if (
+			!Array.isArray(existingStackResponse.Stacks) ||
+			!existingStackResponse.Stacks.length
+		) {
+			throw new Error(
+				"No CloudFormation stacks named GrowYourList found. Are you sure you're connected to the right AWS account?"
 			);
-			await loadConfig();
-			const cloudformation = new AWS.CloudFormation();
-			const existingStackResponse = await cloudformation
-				.describeStacks({ StackName: 'GrowYourList' })
-				.promise();
-			if (
-				!Array.isArray(existingStackResponse.Stacks) ||
-				!existingStackResponse.Stacks.length
-			) {
-				throw new Error(
-					"No CloudFormation stacks named GrowYourList found. Are you sure you're connected to the right AWS account?"
-				);
+		}
+		if (existingStackResponse.Stacks.length !== 1) {
+			throw new Error(
+				'More than 1 GrowYourList stack found. Not sure what to do.'
+			);
+		}
+		const existingStack = existingStackResponse.Stacks[0];
+		if (existingStack.Parameters.length < 16) {
+			throw new Error('Existing stack has an unexpected number of paramters');
+		}
+		const checkParams = existingStack.Parameters.filter((p) => {
+			switch (p.ParameterKey) {
+				case 'AdminEmail':
+				case 'DbTablePrefix':
+				case 'SesSourceEmail':
+				case 'GylEc2InstanceType':
+				case 'GylVersion':
+					return true;
+				default:
+					return false;
 			}
-			if (existingStackResponse.Stacks.length !== 1) {
-				throw new Error(
-					'More than 1 GrowYourList stack found. Not sure what to do.'
-				);
-			}
-			const existingStack = existingStackResponse.Stacks[0];
-			if (existingStack.Parameters.length < 16) {
-				throw new Error('Existing stack has an unexpected number of paramters');
-			}
-			const checkParams = existingStack.Parameters.filter((p) => {
-				switch (p.ParameterKey) {
-					case 'AdminEmail':
-					case 'DbTablePrefix':
-					case 'SesSourceEmail':
-					case 'GylEc2InstanceType':
-					case 'GylVersion':
-						return true;
-					default:
-						return false;
-				}
-			});
-			if (checkParams.length !== 5) {
-				throw new Error('Could not find all "check" parameters');
-			}
-			const stackDetails = `${checkParams
-				.map((p) => `  ${p.ParameterKey}: ${p.ParameterValue}`)
-				.join('\n')}`;
-			const isAlreadyUpToDate =
-				checkParams.find((p) => p.ParameterKey === 'GylVersion')
-					.ParameterValue === GylVersion;
-			if (isAlreadyUpToDate) {
-				Logger.log(`Found GrowYourList stack with the following details:
+		});
+		if (checkParams.length !== 5) {
+			throw new Error('Could not find all "check" parameters');
+		}
+		const stackDetails = `${checkParams
+			.map((p) => `  ${p.ParameterKey}: ${p.ParameterValue}`)
+			.join('\n')}`;
+		const isAlreadyUpToDate =
+			checkParams.find((p) => p.ParameterKey === 'GylVersion')
+				.ParameterValue === GylVersion;
+		if (isAlreadyUpToDate) {
+			Logger.log(`Found GrowYourList stack with the following details:
 ${stackDetails}
 GrowYourList is already up to date (version ${GylVersion}) ✔`);
-				return;
-			}
-			Logger.log(`Found the following GrowYourList stack to update (you can change some of these details during the update):
+			return;
+		}
+		Logger.log(`Found the following GrowYourList stack to update (you can change some of these details during the update):
 ${stackDetails}
 This stack will be updated to GYL version ${GylVersion}`);
-			const SesSourceEmailOld = checkParams.find(
-				(p) => p.ParameterKey === 'SesSourceEmail'
-			).ParameterValue;
-			const SesSourceEmail = await getSesSourceEmail({
-				currentValue: SesSourceEmailOld,
-			});
-			const LambdaBucketName = existingStack.Parameters.find(
-				(p) => p.ParameterKey === 'LambdaBucketName'
-			).ParameterValue;
-			const GylEc2InstanceTypeOld = existingStack.Parameters.find(
-				(p) => p.ParameterKey === 'GylEc2InstanceType'
-			).ParameterValue;
-			const Ec2InstanceType = await getEc2InstanceType({
-				currentValue: GylEc2InstanceTypeOld,
-			});
-			const ApiAuthKeyHash = existingStack.Parameters.find(
-				(p) => p.ParameterKey === 'ApiAuthKeyHash'
-			).ParameterValue;
-			const DbTablePrefix = existingStack.Parameters.find(
-				(p) => p.ParameterKey === 'DbTablePrefix'
-			).ParameterValue;
-			const QueueUser = {
-				accessKeyId: existingStack.Parameters.find(
-					(p) => p.ParameterKey === 'QueueUserAccessKeyId'
-				).ParameterValue,
-				secretAccessKey: existingStack.Parameters.find(
-					(p) => p.ParameterKey === 'QueueUserSecretAccessKey'
-				).ParameterValue,
-			};
-			const BroadcastUser = {
-				accessKeyId: existingStack.Parameters.find(
-					(p) => p.ParameterKey === 'BroadcastUserAccessKeyId'
-				).ParameterValue,
-				secretAccessKey: existingStack.Parameters.find(
-					(p) => p.ParameterKey === 'BroadcastUserSecretAccessKey'
-				).ParameterValue,
-			};
-			const GylVersionOld = existingStack.Parameters.find(
-				(p) => p.ParameterKey === 'GylVersion'
-			).ParameterValue;
-			const AdminEmailOld = existingStack.Parameters.find(
-				(p) => p.ParameterKey === 'AdminEmail'
-			).ParameterValue;
-			const AdminEmail = await getAdminEmail({ currentValue: AdminEmailOld });
-			Logger.log(`About to make the following updates:
-  ${showChange('SesSourceEmail', SesSourceEmailOld, SesSourceEmail)}
-  ${showChange('AdminEmail', AdminEmailOld, AdminEmail)}
-  ${showChange('GylEc2InstanceType', GylEc2InstanceTypeOld, Ec2InstanceType)}
-  ${showChange('GylVersion', GylVersionOld, GylVersion)}`);
-			// const confirmChanges = await getUserInput(
-			// 	'Continue? [y/any key to exit]: '
-			// );
-			// if (confirmChanges.toLocaleLowerCase() !== 'y') {
-			// 	Logger.log('Exiting update process.');
-			// 	return;
-			// }
-
-			Logger.log(
-				'\n## Uploading new version of GYL Software ##\n' +
-					`GYL ${GylVersion} will now be uploaded to your AWS ` +
-					'account. This can take some time.\n'
-			);
-
-			// Logger.log(LambdaBucketName);
-			// Logger.log(ApiAuthKeyHash);
-			// Logger.log(DbTablePrefix);
-			// Logger.log(SesSourceEmail);
-			// Logger.log(QueueUser);
-			// Logger.log(BroadcastUser);
-			// Logger.log(AdminEmail);
-			// Logger.log(Ec2InstanceType);
-			// Logger.log(GylVersion);
-
-			const outputs = await updateCloudFormationStack({
-				LambdaBucketName,
-				ApiAuthKeyHash,
-				DbTablePrefix,
-				SesSourceEmail,
-				QueueUser,
-				BroadcastUser,
-				AdminEmail,
-				Ec2InstanceType,
-				GylVersion,
-			});
-		}
-		const cf = new AWS.CloudFormation();
-		const res1 = await cf
-			.describeStacks({
-				StackName: 'GrowYourList',
-			})
-			.promise();
-		const st1 = res1.Stacks[0];
-		const outputs = {};
-		st1.Outputs.forEach((output) => {
-			outputs[output.Description] = output.OutputValue;
+		const SesSourceEmailOld = checkParams.find(
+			(p) => p.ParameterKey === 'SesSourceEmail'
+		).ParameterValue;
+		const SesSourceEmail = await getSesSourceEmail({
+			currentValue: SesSourceEmailOld,
 		});
+		const LambdaBucketName = existingStack.Parameters.find(
+			(p) => p.ParameterKey === 'LambdaBucketName'
+		).ParameterValue;
+		const GylEc2InstanceTypeOld = existingStack.Parameters.find(
+			(p) => p.ParameterKey === 'GylEc2InstanceType'
+		).ParameterValue;
+		const Ec2InstanceType = await getEc2InstanceType({
+			currentValue: GylEc2InstanceTypeOld,
+		});
+		const ApiAuthKeyHash = existingStack.Parameters.find(
+			(p) => p.ParameterKey === 'ApiAuthKeyHash'
+		).ParameterValue;
+		const DbTablePrefix = existingStack.Parameters.find(
+			(p) => p.ParameterKey === 'DbTablePrefix'
+		).ParameterValue;
+		const QueueUser = {
+			accessKeyId: existingStack.Parameters.find(
+				(p) => p.ParameterKey === 'QueueUserAccessKeyId'
+			).ParameterValue,
+			secretAccessKey: existingStack.Parameters.find(
+				(p) => p.ParameterKey === 'QueueUserSecretAccessKey'
+			).ParameterValue,
+		};
+		const BroadcastUser = {
+			accessKeyId: existingStack.Parameters.find(
+				(p) => p.ParameterKey === 'BroadcastUserAccessKeyId'
+			).ParameterValue,
+			secretAccessKey: existingStack.Parameters.find(
+				(p) => p.ParameterKey === 'BroadcastUserSecretAccessKey'
+			).ParameterValue,
+		};
+		const GylVersionOld = existingStack.Parameters.find(
+			(p) => p.ParameterKey === 'GylVersion'
+		).ParameterValue;
+		const AdminEmailOld = existingStack.Parameters.find(
+			(p) => p.ParameterKey === 'AdminEmail'
+		).ParameterValue;
+		const AdminEmail = await getAdminEmail({ currentValue: AdminEmailOld });
+		Logger.log(`About to make the following updates:
+${showChange('SesSourceEmail', SesSourceEmailOld, SesSourceEmail)}
+${showChange('AdminEmail', AdminEmailOld, AdminEmail)}
+${showChange('GylEc2InstanceType', GylEc2InstanceTypeOld, Ec2InstanceType)}
+${showChange('GylVersion', GylVersionOld, GylVersion)}`);
+		// const confirmChanges = await getUserInput(
+		// 	'Continue? [y/any key to exit]: '
+		// );
+		// if (confirmChanges.toLocaleLowerCase() !== 'y') {
+		// 	Logger.log('Exiting update process.');
+		// 	return;
+		// }
+
+		Logger.log(
+			'\n## Uploading new version of GYL Software ##\n' +
+				`GYL ${GylVersion} will now be uploaded to your AWS ` +
+				'account. This can take some time.\n'
+		);
+
+		// Logger.log(LambdaBucketName);
+		// Logger.log(ApiAuthKeyHash);
+		// Logger.log(DbTablePrefix);
+		// Logger.log(SesSourceEmail);
+		// Logger.log(QueueUser);
+		// Logger.log(BroadcastUser);
+		// Logger.log(AdminEmail);
+		// Logger.log(Ec2InstanceType);
+		// Logger.log(GylVersion);
+
+		const outputs = await updateCloudFormationStack({
+			LambdaBucketName,
+			ApiAuthKeyHash,
+			DbTablePrefix,
+			SesSourceEmail,
+			QueueUser,
+			BroadcastUser,
+			AdminEmail,
+			Ec2InstanceType,
+			GylVersion,
+		});
+		// const cf = new AWS.CloudFormation();
+		// const res1 = await cf
+		// 	.describeStacks({
+		// 		StackName: 'GrowYourList',
+		// 	})
+		// 	.promise();
+		// const st1 = res1.Stacks[0];
+		// const outputs = {};
+		// st1.Outputs.forEach((output) => {
+		// 	outputs[output.Description] = output.OutputValue;
+		// });
 		// Logger.log('OUTPUTS');
 		// Logger.log(outputs);
 		await updateSesEventDestinations(outputs);
